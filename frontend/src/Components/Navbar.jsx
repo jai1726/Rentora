ecf9df60388106a884fd19c6959d0ef40830aa5e
Pavan <pavankumarrouthu761@gmail.com>
2024-10-23 00:16:12 +0530

    Basic implimentation of Navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa'; // Importing user icon from react-icons
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4 flex justify-between items-center">
+      <div className="text-white">
+        <a href="/" className="text-lg font-semibold">Home</a>
+      </div>
+      <div className="text-white">
+        <FaUserCircle size={30} />
+      </div>
+    </nav>
+  );
+}

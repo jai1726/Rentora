cadb1ff1982a53382569f2540adb7a2d955fcfd1
jai1726 <jagadeeshdende@gmail.com>
2024-10-23 15:11:55 +0530

    delete post

--- backend/controllers/deletePost.js
+++ backend/controllers/deletePost.js
@@ -5,24 +5,23 @@ const User = require('../models/User.models.js');
 
 
 const deletePost = async (req, res, next) => {
-    // const {user}=re.body;
+
     const postId = req.params.id.trim();
     let post;
 
     try {
-        // Find the post and populate the 'user' field
+        
         console.log(postId)
         post = await Post.findById(postId).populate('user');
         if (!post) {
             return res.status(404).json({ message: "Post not found" });
         }
 
-        // Remove the post reference from the user's posts array
         const user = post.user;
-        user.posts.pull(post._id); // Use post._id instead of post object
+        user.posts.pull(post._id); 
         await user.save();
 
-        // Delete the post
+        
         await Post.findByIdAndDelete(postId);
 
         res.status(200).json({ message: "Successfully deleted" });

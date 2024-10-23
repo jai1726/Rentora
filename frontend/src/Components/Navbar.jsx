1863f849589a6cb1dedb1db898287845f3c42ec2
Pavan <pavankumarrouthu761@gmail.com>
2024-10-23 10:18:30 +0530

    Changed the Navbar Color

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import { FaUserCircle } from 'react-icons/fa'; // Importing user icon from react
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 p-4 flex justify-between items-center">
+    <nav className="bg-purple-400 p-4 flex justify-between items-center">
       <div className="text-white">
-        <a href="/" className="text-lg font-semibold">Home</a>
+        <a href="/" className="text-lg font-semibold">Rentora.</a>
       </div>
       <div className="text-white">
         <FaUserCircle size={30} />

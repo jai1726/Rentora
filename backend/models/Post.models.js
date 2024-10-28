61cd4128f8947f517db1ff6051dc2d8d183ae1be
jai1726 <jagadeeshdende@gmail.com>
2024-10-28 23:58:08 +0530

    correction of  database

--- backend/models/Post.models.js
+++ backend/models/Post.models.js
@@ -3,13 +3,12 @@ const mongoose =require('mongoose');
 const postSchema= new mongoose.Schema({
     description:{
         type:String,
-        required:true,
-        unique:true
+        required:true
        },
        images:{
-        type:String,
-        required:true 
-       },
+        type: [String],   // Array of strings to store image URLs
+        validate: [arrayLimit, '{PATH} must have exactly 3 images'],
+    },
        location:{
            type:String,
            required:true 
@@ -28,6 +27,10 @@ const postSchema= new mongoose.Schema({
         ref:"User",
         required:true
        }
+       
 })
+function arrayLimit(val) {
+    return val.length === 3; // Ensures the array has exactly 3 elements
+}
 const Post= mongoose.model("Post",postSchema);
 module.exports = Post;

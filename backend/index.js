272f966cfe093c8a914a87c137f9ea09796612d5
jai1726 <jagadeeshdende@gmail.com>
2024-12-03 00:41:02 +0530

    Created Socket.io for RealTime Messaging

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,22 @@ const connectDB = require('./db.js');
 const cors = require('cors');
 const router = require('./routes/User.Routes.js');
 const postRouter = require('./routes/Post.Routes.js');
-
+const http = require('http');
+const { Server } = require('socket.io');
+const setupMessagingSockets = require('./sockets/messaging.js');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+const server=http.createServer(app);
+const io=new Server(server, {
+    cors:{
+        origin:'http://localhost:3000',
+        methods:['GET','POST'],
+    },
+});
 app.use(express.json());
 app.use(cors());
 
@@ -21,7 +30,7 @@ app.use("/api/user", router);
 app.use("/api/post", postRouter);
 
 
-
+setupMessagingSockets(io);
 
 // app.use('/', (req, res) => {
 //     console.log("GET request received");
